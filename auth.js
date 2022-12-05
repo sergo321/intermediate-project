964b0295bb87cb6b49db4d777cd6a28fd829b68d
sergo321 <smuradash1@gmail.com>
2022-12-06 01:12:11 +0400

    dasasrulebeli db ze kavshiri

--- auth.js
+++ auth.js
@@ -1,66 +1,62 @@
-// const signUp = document.querySelector("#signUp");
-// const singIn = document.querySelector("#singIn");
-// const register = document.querySelector("#register");
-// const login = document.querySelector("#login");
-// const changePasswordVisibility = document.querySelector("#changePasswordVisibility");
-// const actionbutton = document.querySelector("#actionbutton");
-// const actionToggleArray = document.querySelectorAll(".actionToggle");
-// const topSide = document.querySelector(".top");
+const signUp = document.querySelector("#signUp");
+const singIn = document.querySelector("#singIn");
+const register = document.querySelector("#register");
+const login = document.querySelector("#login");
+const changePasswordVisibility = document.querySelector("#changePasswordVisibility");
+const registerbutton = document.querySelector("#registerbutton");
+const actionToggleArray = document.querySelectorAll(".actionToggle");
+const topSide = document.querySelector(".top");
 
-// const name = document.querySelector("#name");
-// const lastname = document.querySelector("#lastName");
-// const email = document.querySelector("#email");
-// const password = document.querySelector("#password");
 
-// const url = location.href;
-// const action = url.split("/").pop().split("?").pop() || 'register';
 
-// const showPasswordIcon = '<i class="fa-solid fa-eye"></i>';
-// const hidePasswordIcon = '<i class="fa-solid fa-eye-slash"></i>';
-// let isShownPassword = true;
+const lastname = document.querySelector("#fullname");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
 
-// if (action === "register") {
-//   toggle('register');
-// } else {
-//   toggle('login');
-// }
+const url = location.href;
+const action = url.split("/").pop().split("?").pop() || 'register';
 
-// changePasswordVisibility.addEventListener("click", () => {
-//   if (isShownPassword) {
-//     changePasswordVisibility.innerHTML = hidePasswordIcon;
-//     password.type = 'text';
-//   } else {
-//     changePasswordVisibility.innerHTML = showPasswordIcon;
-//     password.type = 'password';
-//   }
-//   isShownPassword = !isShownPassword;
-// });
+const showPasswordIcon = '<i class="fa-solid fa-eye"></i>';
+const hidePasswordIcon = '<i class="fa-solid fa-eye-slash"></i>';
+let isShownPassword = true;
 
-// register.addEventListener("click", () => {
-//   toggle('register');
-// });
+if (action === "register") {
+  toggle('register');
+  
+} else {
+  toggle('login');
+  registerbutton.innerHTML="log in"
+}
 
-// login.addEventListener("click", () => {
-//   toggle('login');
-// });
+login.addEventListener("click",()=>{
+    registerbutton.innerHTML="log in" 
+})
 
-// function toggle(action) {
-//   const isAction = action === 'register';
+register.addEventListener("click",()=>{
+    registerbutton.innerHTML="register" 
+})
+changePasswordVisibility.addEventListener("click", () => {
+  if (isShownPassword) {
+    changePasswordVisibility.innerHTML = hidePasswordIcon;
+    password.type = 'text';
+  } else {
+    changePasswordVisibility.innerHTML = showPasswordIcon;
+    password.type = 'password';
+  }
+  isShownPassword = !isShownPassword;
+});
 
-//   if (isAction) {
-//     signUp.classList.add('active');
-//     singIn.classList.remove('active');
-//   } else {
-//     signUp.classList.remove('active');
-//     singIn.classList.add('active');
-//   }
+register.addEventListener("click", () => {
+  toggle('register');
+});
 
-//   actionToggleArray.forEach((element) => {
-//     element.style.display = isAction ? 'block' : 'none';
-//   });
-//   actionbutton.classList.add(isAction ? 'btn-primary' : 'btn-warning');
-//   actionbutton.classList.remove(isAction ? 'btn-warning' : 'btn-primary');
-//   actionbutton.textContent = isAction ? 'Sign up' : 'Sing in';
-//   login.style.color = isAction ? 'black' : '#ffc107';
-//   register.style.color = isAction ? '#0d6efd' : 'black';
-// }
+login.addEventListener("click", () => {
+  toggle('login');
+});
+
+function toggle(action) {
+  const isAction = action === 'register';
+  actionToggleArray.forEach((element) => {
+    element.style.display = isAction ? 'block' : 'none';
+  });
+}
--- firebase.js
+++ firebase.js
@@ -10,14 +10,25 @@ const firebaseConfig = {
   };
 
 
+
+  function randomID() {
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+      let r = (Math.random() * 16) | 0;
+      let v = c == "x" ? r : (r & 0x3) | 0x8;
+      return v.toString(16);
+    });
+  }
+
+
+
   firebase.initializeApp(firebaseConfig); //დაკავშირება firebase სთან , არსებული კონფიგით
 
-/**
- * ფაირბეისიდან დაბრუნებული ინფორმაციის ობიექტში გადაკეთება
- * @param ID - უნიკალური იდ
- * @param value - მნიშვნელობა რაც არის მონაცემთა ბაზაში
- * @returns ვაბრუნებთ მთლიან ობიექტს სადაცა რის ID და Value
- */
+// /**
+//  * ფაირბეისიდან დაბრუნებული ინფორმაციის ობიექტში გადაკეთება
+//  * @param ID - უნიკალური იდ
+//  * @param value - მნიშვნელობა რაც არის მონაცემთა ბაზაში
+//  * @returns ვაბრუნებთ მთლიან ობიექტს სადაცა რის ID და Value
+//  */
 function generateFirebaseItem(ID, value) {
   return {
     id: ID,
@@ -25,73 +36,79 @@ function generateFirebaseItem(ID, value) {
   }
 }
 
-/**
- * დავამატოთ მონაცემთა ბაზაი ელემენტი
- * @param REF - დასახელება მონაცემთა ბაზის განშტოების
- * @param data - ინფორმაცია რასაც ვამატებთ
- */
+// /**
+//  * დავამატოთ მონაცემთა ბაზაი ელემენტი
+//  * @param REF - დასახელება მონაცემთა ბაზის განშტოების
+//  * @param data - ინფორმაცია რასაც ვამატებთ
+//  */
 function addElementInFirebase(REF, data) {
   firebase.database().ref(`${REF}/${randomID()}`).set(data);
 }
 
-/**
- * მთლიანი განშტოების წამოღება ფაირბეისიდან
- * @param REF - დასახელება მონაცემთა ბაზის განშტოების
- * @returns აბრუნებს განშტოებაზე არსებულ ინფორმაციას
- */
-function getRefFromFirebase(REF) {
-  const result = [];
-  firebase.database().ref(REF).on("value", (response) => {
-    response.forEach((element) => {
-      result.push(generateFirebaseItem(element.key, element.val()));
-    });
-  });
-  return result;
-}
 
-/**
- * კონკრეტული ელემენტის დაბრუნება განშტოებიდან
- * @param REF - დასახალება მონაცემთა ბაზის განშტოების
- * @param id - განშტოებაზე არესბული ელემენტის უნიკალური იდ
- * @returns აბრუნებს Promise კარგ შემთხვევაში მონაცემს ცუდ შემთხვევაში "404"
- */
-function getElementFromFirebase(REF, id) {
-  const array = getArrayFromFirebase(REF);
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      array.forEach((element) => {
-        if (element.id === id) {
-          resolve(element);
-        }
-      });
-      reject("404");
-    }, 1000);
-  });
-}
+addElementInFirebase("user",{
+  name:"test1",
+  lastname:"test2"
+})
 
-/**
- * გავანახლოთ ინფორმაცია ფაირბეისში
- * @param REF - დასახალება მონაცემთა ბაზის განშტოების
- * @param id - განშტოებაზე არესბული ელემენტის უნიკალური იდ
- * @param data - განახლებული ინფორმაცია
- */
-function updateDataInFirebaseByID(REF, id, data) {
-  firebase.database().ref(`${REF}/${id}`).set(data);
-}
+// /**
+//  * მთლიანი განშტოების წამოღება ფაირბეისიდან
+//  * @param REF - დასახელება მონაცემთა ბაზის განშტოების
+//  * @returns აბრუნებს განშტოებაზე არსებულ ინფორმაციას
+//  */
+// function getRefFromFirebase(REF) {
+//   const result = [];
+//   firebase.database().ref(REF).on("value", (response) => {
+//     response.forEach((element) => {
+//       result.push(generateFirebaseItem(element.key, element.val()));
+//     });
+//   });
+//   return result;
+// }
 
-/**
- * განშტოებიდან ელემენტის ამოშლა
- * @param REF - დასახალება მონაცემთა ბაზის განშტოების
- * @param id - განშტოებაზე არესბული ელემენტის უნიკალური იდ
- */
-function removeElementFromFirebase(REF, id) {
-  firabase.database().ref(`${REF}/${id}`).remove();
-}
+// /**
+//  * კონკრეტული ელემენტის დაბრუნება განშტოებიდან
+//  * @param REF - დასახალება მონაცემთა ბაზის განშტოების
+//  * @param id - განშტოებაზე არესბული ელემენტის უნიკალური იდ
+//  * @returns აბრუნებს Promise კარგ შემთხვევაში მონაცემს ცუდ შემთხვევაში "404"
+//  */
+// function getElementFromFirebase(REF, id) {
+//   const array = getArrayFromFirebase(REF);
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       array.forEach((element) => {
+//         if (element.id === id) {
+//           resolve(element);
+//         }
+//       });
+//       reject("404");
+//     }, 1000);
+//   });
+// }
 
-/**
- * მთლიანი განშტოების წაშლა
- * @param REF - დასახალება მონაცემთა ბაზის განშტოების
- */
-function removeRefFromFirebase(REF) {
-  firebase.database().ref(REF).remove();
-}
+// /**
+//  * გავანახლოთ ინფორმაცია ფაირბეისში
+//  * @param REF - დასახალება მონაცემთა ბაზის განშტოების
+//  * @param id - განშტოებაზე არესბული ელემენტის უნიკალური იდ
+//  * @param data - განახლებული ინფორმაცია
+//  */
+// function updateDataInFirebaseByID(REF, id, data) {
+//   firebase.database().ref(`${REF}/${id}`).set(data);
+// }
+
+// /**
+//  * განშტოებიდან ელემენტის ამოშლა
+//  * @param REF - დასახალება მონაცემთა ბაზის განშტოების
+//  * @param id - განშტოებაზე არესბული ელემენტის უნიკალური იდ
+//  */
+// function removeElementFromFirebase(REF, id) {
+//   firabase.database().ref(`${REF}/${id}`).remove();
+// }
+
+// /**
+//  * მთლიანი განშტოების წაშლა
+//  * @param REF - დასახალება მონაცემთა ბაზის განშტოების
+//  */
+// function removeRefFromFirebase(REF) {
+//   firebase.database().ref(REF).remove();
+// }
