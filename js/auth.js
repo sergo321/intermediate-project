90386928bc4a8e2f6df54cf661dbc718669e3d2e
georgeneligan <108015805+georgeneligan@users.noreply.github.com>
2022-12-07 15:58:34 +0400

    update auth.html, style.css, auth.js

--- js/auth.js
+++ js/auth.js
@@ -2,68 +2,62 @@ const signUp = document.querySelector("#signUp");
 const singIn = document.querySelector("#singIn");
 const register = document.querySelector("#register");
 const login = document.querySelector("#login");
-const changePasswordVisibility = document.querySelector("#changePasswordVisibility");
+const changePasswordVisibility = document.querySelector(
+  "#changePasswordVisibility"
+);
 const registerbutton = document.querySelector("#registerbutton");
 const actionToggleArray = document.querySelectorAll(".actionToggle");
 const topSide = document.querySelector(".top");
-
-
 
 const lastname = document.querySelector("#fullname");
 const email = document.querySelector("#email");
 const password = document.querySelector("#password");
 
 const url = location.href;
-const action = url.split("/").pop().split("?").pop() || 'register';
+const action = url.split("/").pop().split("?").pop() || "register";
 
 const showPasswordIcon = '<i class="fa-solid fa-eye"></i>';
 const hidePasswordIcon = '<i class="fa-solid fa-eye-slash"></i>';
 let isShownPassword = true;
 
 if (action === "register") {
-  toggle('register');
-  
+  toggle("register");
 } else {
-  toggle('login');
-  registerbutton.innerHTML="log in"
+  toggle("login");
+  registerbutton.innerHTML = "Log in";
 }
 
-login.addEventListener("click",()=>{
-    registerbutton.innerHTML="log in" 
-})
+login.addEventListener("click", () => {
+  registerbutton.innerHTML = "Log in";
+});
 
-register.addEventListener("click",()=>{
-    registerbutton.innerHTML="register" 
-})
+register.addEventListener("click", () => {
+  registerbutton.innerHTML = "Register";
+});
 changePasswordVisibility.addEventListener("click", () => {
   if (isShownPassword) {
     changePasswordVisibility.innerHTML = hidePasswordIcon;
-    password.type = 'text';
+    password.type = "text";
   } else {
     changePasswordVisibility.innerHTML = showPasswordIcon;
-    password.type = 'password';
+    password.type = "password";
   }
   isShownPassword = !isShownPassword;
 });
 
 register.addEventListener("click", () => {
-  toggle('register');
+  toggle("register");
 });
 
 login.addEventListener("click", () => {
-  toggle('login');
+  toggle("login");
 });
 
 function toggle(action) {
-  const isAction = action === 'register';
+  const isAction = action === "register";
   actionToggleArray.forEach((element) => {
-    if(element.style.display = isAction){
-     element.style.display= 'block'
-    }else(element.style.display='none')
+    if ((element.style.display = isAction)) {
+      element.style.display = "block";
+    } else element.style.display = "none";
   });
 }
-
-
-
-
-
