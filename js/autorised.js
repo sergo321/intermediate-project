39e3bbe0d3db1078af0ae6bc76e8026963b84828
sergo321 <smuradash1@gmail.com>
2023-01-16 19:14:53 +0400

    bag fix error 404

--- js/autorised.js
+++ js/autorised.js
@@ -1,9 +1,8 @@
 let logout=document.querySelector("#logout");
 
 
-
 logout.addEventListener("click",()=>{
     sessionStorage.clear;
     localStorage.clear;
-    location.href="../auth.html"
+    location.href="./auth.html"
 })
