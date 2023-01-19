304887d97e0bd33fa11363682600e717bfeac23c
sergo321 <smuradash1@gmail.com>
2023-01-20 01:38:41 +0400

    gasasworebelia

--- js/autorised.js
+++ js/autorised.js
@@ -6,3 +6,32 @@ logout.addEventListener("click",()=>{
     localStorage.clear();
     location.href="./auth.html"
 })
+
+
+
+// for coment
+
+
+// let displayarea=document.querySelector("#displayarea");
+
+
+// btnforcom.addEventListener("click", ()=>{
+//     let named=nameforcom.value;
+//     let maild=mailforcom.value;
+//     let phoned=phoneforcom.value;
+//     let adressd=adressforcom.value;
+//     let subjectd=subjectforcom.value;
+//     let messiged=messigeforcom.value;
+//     if (named===""|| maild===""||phoned===""||adressd===""||subjectd===""||messiged===""){
+//     console.log("")
+//     }else {
+// displayarea.innerHTML=displayarea.innerHTML+`
+// <p><span>${named}</span><span>${maild}</span></p>
+//         <p><span>${phoned}</span><span>${adressd}</span></p>
+//         <p>${subjectd}}</p>
+//         <p>${messiged}</p>
+//         <button id="delated">delete</button>
+// `}
+// })
+
+//
