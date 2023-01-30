ff732644d0d4f9d4db2240e1850e13daa353b034
sergo321 <smuradash1@gmail.com>
2023-01-30 19:25:07 +0400

    add post part only html and js

--- js/addpost.js
+++ js/addpost.js
@@ -1,41 +1,23 @@
-let nameforcom=document.querySelector("#name");
-let mailforcom=document.querySelector("#mail");
-let phoneforcom=document.querySelector("#phone");
-let adressforcom=document.querySelector("#adress");
-let subjectforcom=document.querySelector("#sublect");
-let messigeforcom=document.querySelector("#messige");
-
-let btnforcom=document.querySelector("#btnforcom")
+const display=document.querySelector("#display");
+const remainder=document.querySelector("#remainder");
+const letter=document.querySelector("#letter");
+const makepost=document.querySelector("#makepost")
 
 
 
-
-
-btnforcom.addEventListener("click",()=>{
-    let namebase = nameforcom.value;
-let mailbase=mailforcom.value;
-let phonebase=phoneforcom.value;
-let adressbase=adressforcom.value;
-let subjectbase=subjectforcom.value;
-let messigebase=messigeforcom.value;
-    if (namebase===""|| mailbase===""||phonebase===""||adressbase===""||subjectbase===""||messigebase===""){
-        alert("fill all value")
-    }else {
-     addElementInFirebase("contact", {
-            name: namebase,
-            email: mailbase,
-            phone:phonebase,
-            adress:adressbase,
-            subject:subjectbase,
-            messige:messigebase,
-          });
-         setTimeout(function clearfunction(){
-            nameforcom.value="";
-          mailforcom.value="";
-          phoneforcom.value="";
-          adressforcom.value="";
-          subjectforcom.value="";
-          messigeforcom.value="";},500)
-    }
+makepost.addEventListener("click", ()=>{
+   let remaindername=remainder.value;
+   let remainderletter=letter.value;
+    display.innerHTML=display.innerHTML+`<div id="postwrapper">
+    <p class="remaindername">${remaindername}</p>
+    <p class="remainderletter">${remainderletter}</p>
+  
+  <button id="deleteletter" onclick="delateletter()">delate</button>
+  </div>`
+  remainder.value="";
+  letter.value="";
 })
 
+function delateletter(){
+        const postwrapper=document.querySelector("#postwrapper").remove();  
+}
--- js/autorised.js
+++ js/autorised.js
@@ -9,29 +9,3 @@ logout.addEventListener("click",()=>{
 
 
 
-// for coment
-
-
-// let displayarea=document.querySelector("#displayarea");
-
-
-// btnforcom.addEventListener("click", ()=>{
-//     let named=nameforcom.value;
-//     let maild=mailforcom.value;
-//     let phoned=phoneforcom.value;
-//     let adressd=adressforcom.value;
-//     let subjectd=subjectforcom.value;
-//     let messiged=messigeforcom.value;
-//     if (named===""|| maild===""||phoned===""||adressd===""||subjectd===""||messiged===""){
-//     console.log("")
-//     }else {
-// displayarea.innerHTML=displayarea.innerHTML+`
-// <p><span>${named}</span><span>${maild}</span></p>
-//         <p><span>${phoned}</span><span>${adressd}</span></p>
-//         <p>${subjectd}}</p>
-//         <p>${messiged}</p>
-//         <button id="delated">delete</button>
-// `}
-// })
-
-//
